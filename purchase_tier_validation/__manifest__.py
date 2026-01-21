584a1371e1c6f8b5cc836b5c95fb4a6066008760
jaydeep32 <jaydipgajera2020@gmail.com>
2026-01-21 15:11:33 -0500

    [IMP] purchase_tier_validation: pre-commit auto fixes

--- purchase_tier_validation/__manifest__.py
+++ purchase_tier_validation/__manifest__.py
@@ -6,7 +6,7 @@
     "support a tier validation process.",
     "version": "18.0.1.0.0",
     "category": "Purchases",
-    "website": "https://github.com/OCA/purchase-workflow",
+    "website": "https://github.com/OCA/tier-validation",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
